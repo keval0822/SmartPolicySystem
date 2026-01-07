b0b09ac7402cd85292bbabfdd4da451b31e3c23e
keval0822 <74790790+keval0822@users.noreply.github.com>
2026-01-07 22:26:58 +0530

    Add files via upload

---
+++ Bronze/BronzeLayer-DBCreation.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %sql 
+# MAGIC select current_database()
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC
+# MAGIC create database bronzelayer
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC
+# MAGIC use bronzelayer
+
+# COMMAND ----------
+
